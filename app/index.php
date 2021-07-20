32a1052c6cfc0bcd5f4b2aa1691ef212bdaba991
Mitsu <mitsukarenai@users.noreply.github.com>
2021-07-21 00:37:12 +0200

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    Test: 
+    <?php
+    echo date(DATE_RFC2822);
+    ?>
+    
+  </body>
+</html>
